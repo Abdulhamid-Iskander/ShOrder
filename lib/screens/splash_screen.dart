1e4888f2510e2a095bf902333ca239d7baa58466
Abdulhamid-Iskander <abdulhamidiskander@gmail.com>
2025-08-28 17:28:47 +0300

    add splash screen

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,10 +1,64 @@
 import 'package:flutter/material.dart';
+import 'login.dart';
 
-class SplashScreen extends StatelessWidget {
-  const SplashScreen({super.key});
+class SplashScreen extends StatefulWidget {
+  final int durationInSconds;
+  final bool enableAnimation;
+  const SplashScreen({
+    super.key,
+    this.durationInSconds = 3,
+    this.enableAnimation = true,
+  });
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+  @override
+  void initState() {
+    super.initState();
+    if (widget.enableAnimation) {
+      _controller = AnimationController(
+        duration: Duration(seconds: widget.durationInSconds),
+        vsync: this,
+      );
+      _animation = CurvedAnimation(parent: _controller, curve: Curves.easeIn);
+      _controller.forward();
+    }
+  }
+
+  @override
+  void dispose() {
+    if (widget.enableAnimation) {
+      _controller.dispose();
+    }
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    Future.delayed(Duration(seconds: widget.durationInSconds), () {
+      Navigator.of(
+        context,
+      ).pushReplacement(MaterialPageRoute(builder: (context) => const Login()));
+    });
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: widget.enableAnimation
+            ? FadeTransition(
+                opacity: _animation,
+                child: Image.asset(
+                  'assets/images/logo.png',
+                  width: 200,
+                  height: 200,
+                ),
+              )
+            : Image.asset('assets/images/logo.png', width: 200, height: 200),
+      ),
+    );
   }
 }
